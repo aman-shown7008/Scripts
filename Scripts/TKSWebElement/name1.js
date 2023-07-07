a2ee718d7774ccc242087fb7f2ff670275802b6d
Aman Mishra <74037648+aman-shown7008@users.noreply.github.com>
2023-07-07 12:21:11 +0530

    Delete Scripts directory

--- Scripts/TKSWebElement/name1.js
+++
@@ -1,9 +0,0 @@
-var elementId = arguments[0];
-var elem = document.getElementById(elementId);
-var rtnValue="Empty";
-
-if( elem.textContent.length > 0) {
-    rtnValue = elem.textContent;
-}
-
-return rtnValue;
--- Scripts/TKSWebMenu/name.js
+++
@@ -1,6 +0,0 @@
-var elementId = arguments[0];
-var elem=document.getElementById(elementId);
-if(elem.getAttribute('class')=='gwt-Label WBUC')
-return;
-var value=elem.parentElement.parentElement.parentElement;
-return value.previousSibling.lastElementChild.innerText;
